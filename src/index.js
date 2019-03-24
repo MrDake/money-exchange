21ae5f9d2676b174484ab5b36c7aaae20767cab5
MrDake <nalulxd@gmail.com>
2019-03-24 16:40:45 +0600

    feat: add working code

--- src/index.js
+++ src/index.js
@@ -2,4 +2,32 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    
+    var coinsValue = {"H":50,"Q":25,"D":10,"N":5,"P":1};
+    var exchangedCoins = {};
+
+    if (currency >= 10000) {
+        
+        exchangedCoins = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    } else if (currency <= 0) {
+
+        exchangedCoins = {};
+
+    } else {
+
+        for (var i in coinsValue) {
+            exchangedCoins[i] = Math.floor(currency/coinsValue[i]);
+            if(exchangedCoins[i] === 0) {
+                delete exchangedCoins[i];
+            } else {
+                currency = currency - coinsValue[i] * exchangedCoins[i];
+                console.log('coin '+coinsValue[i]+':  '+exchangedCoins[i]);
+            }
+        }
+    
+    }
+
+    return exchangedCoins;
+    
 }
